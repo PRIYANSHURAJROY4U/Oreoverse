993683144561f808c1bbc6c34ec06602c775f739
PRIYANSHURAJROY4U <priyanshur.ip.21@nitj.ac.in>
2025-08-30 20:39:54 +0530

    change config.js

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // /* config options here */   check for it
+// const nextConfig: NextConfig = {
+//   // /* config options here */   check for it
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
