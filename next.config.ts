3bda75d7a836b2fa8cfa6329876a0b09c594106a
PRIYANSHURAJROY4U <priyanshur.ip.21@nitj.ac.in>
2025-10-16 15:17:44 +0530

    latest build

--- next.config.ts
+++ next.config.ts
@@ -9,17 +9,25 @@
 
 // export default nextConfig;
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    turbo: false, // Disable Turbopack
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}
 
-module.exports = nextConfig
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   experimental: {
+//     turbo: false, // Disable Turbopack
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {};
+
+export default nextConfig;
